188649a71fbdb2ae7840aa00ee66695074a3fee1
Victor Alcaraz <vctralcaraz@gmail.com>
2023-11-28 23:11:26 -0800

    phase 1 and 2 complete

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+
+server = http.createServer((req, res) => {
+  const method = req.method;
+  const url = req.url;
+
+  console.log(method, url);
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log('Successfully started the server on port', port);
+})
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
